97ca5ff8c2bd1464aa9196be30c8b02c2fee31db
Dang <huynhminhdang@gmail.com>
2017-09-26 00:38:43 +0800

    refactor: use functional component

--- src/Book.js
+++ src/Book.js
@@ -2,52 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {BookType, ShelfType} from './types';
 
-export default class Book extends React.Component {
-  static propTypes = {
-    book: BookType.isRequired,
-    shelfId: PropTypes.string,
-    shelves: PropTypes.arrayOf(ShelfType).isRequired,
-    moveBookToShelf: PropTypes.func.isRequired
-  };
-
-  render() {
-    const {book, shelves, shelfId, moveBookToShelf} = this.props;
-    const {title, authors, imageLinks} = book;
-
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              backgroundImage: `url("${(imageLinks && imageLinks.thumbnail) ||
-                'https://books.google.com.sg/googlebooks/images/no_cover_thumb.gif'}")`
-            }}
-          />
-          <div className="book-shelf-changer">
-            <select
-              value={shelfId}
-              onChange={e => moveBookToShelf(book, e.target.value)}
-            >
-              <option value="none" disabled>
-                Move to...
+function Book({book, shelves, shelfId, moveBookToShelf}) {
+  const {title, authors, imageLinks} = book;
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            backgroundImage: `url("${(imageLinks && imageLinks.thumbnail) ||
+              'https://books.google.com.sg/googlebooks/images/no_cover_thumb.gif'}")`
+          }}
+        />
+        <div className="book-shelf-changer">
+          <select
+            value={shelfId}
+            onChange={e => moveBookToShelf(book, e.target.value)}
+          >
+            <option value="none" disabled>
+              Move to...
+            </option>
+            {shelves.map(shelf => (
+              <option key={shelf.id} value={shelf.id}>
+                {shelf.title}
               </option>
-              {shelves.map(shelf => (
-                <option key={shelf.id} value={shelf.id}>
-                  {shelf.title}
-                </option>
-              ))}
-              <option value="none">None</option>
-            </select>
-          </div>
+            ))}
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{title}</div>
-        {authors && (
-          <div className="book-authors">
-            {authors.map(author => <div key={author}>{author}</div>)}
-          </div>
-        )}
       </div>
-    );
-  }
+      <div className="book-title">{title}</div>
+      {authors && (
+        <div className="book-authors">
+          {authors.map(author => <div key={author}>{author}</div>)}
+        </div>
+      )}
+    </div>
+  );
 }
+
+Book.propTypes = {
+  book: BookType.isRequired,
+  shelfId: PropTypes.string,
+  shelves: PropTypes.arrayOf(ShelfType).isRequired,
+  moveBookToShelf: PropTypes.func.isRequired
+};
+
+export default Book;
--- src/ShelfPage.js
+++ src/ShelfPage.js
@@ -5,41 +5,40 @@ import Bookshelf from './Bookshelf';
 import Book from './Book';
 import {ShelfType} from './types';
 
-export default class ShelfPage extends React.Component {
-  static propTypes = {
-    shelves: PropTypes.arrayOf(ShelfType).isRequired,
-    getBooksInShelf: PropTypes.func.isRequired,
-    moveBookToShelf: PropTypes.func.isRequired
-  };
-
-  render() {
-    const {shelves, getBooksInShelf, moveBookToShelf} = this.props;
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {shelves.map(shelf => (
-              <Bookshelf key={shelf.id} title={shelf.title}>
-                {getBooksInShelf(shelf.id).map(book => (
-                  <Book
-                    key={book.id}
-                    book={book}
-                    shelfId={shelf.id}
-                    shelves={shelves}
-                    moveBookToShelf={moveBookToShelf}
-                  />
-                ))}
-              </Bookshelf>
-            ))}
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
+function ShelfPage({shelves, getBooksInShelf, moveBookToShelf}) {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          {shelves.map(shelf => (
+            <Bookshelf key={shelf.id} title={shelf.title}>
+              {getBooksInShelf(shelf.id).map(book => (
+                <Book
+                  key={book.id}
+                  book={book}
+                  shelfId={shelf.id}
+                  shelves={shelves}
+                  moveBookToShelf={moveBookToShelf}
+                />
+              ))}
+            </Bookshelf>
+          ))}
         </div>
       </div>
-    );
-  }
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  );
 }
+
+ShelfPage.propTypes = {
+  shelves: PropTypes.arrayOf(ShelfType).isRequired,
+  getBooksInShelf: PropTypes.func.isRequired,
+  moveBookToShelf: PropTypes.func.isRequired
+};
+
+export default ShelfPage;
